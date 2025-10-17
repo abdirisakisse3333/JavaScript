5dc57d25ea161c20e15d6c9b52b2722549a9f6fc
abdirisakisse3333 <puntland@abdirizaks-MacBook-Pro.local>
2025-10-17 07:21:32 +0300

    i have added strings

---
+++ strings/script.js
@@ -0,0 +1,18 @@
+// Different ways to create strings
+
+let firstName = 'faarax'; //using single quotes 
+let lastName = "maxamed" // usngig double quotes
+let age = 20;
+let isMarried = false;
+let counrty = 'somalia'
+let city = `USA`; //using templete titerals 
+console.log(firstName,lastName,age,isMarried,counrty); 
+
+//   how to access strings 
+
+console.log(firstName.indexOf("f"));
+console.log(firstName.indexOf("r"));
+console.log(firstName.indexOf("F"));
+
+// String methods
+
